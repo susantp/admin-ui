2499ab2b4924cdb4546a6515d5ab19a8dc03652a
Susant Paudel <susanta.paudel@lisnepal.com.np>
2023-05-30 22:51:02 +0545

    Create Error Boundary component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+
 import "@/styles/globals.css"
 import React from "react"
 import { Metadata } from "next"
 import { NextFont } from "next/dist/compiled/@next/font"
 import { Inter } from "next/font/google"
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 const inter: NextFont = Inter({ subsets: ["latin"] })
 
@@ -18,7 +20,11 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ErrorBoundary>
+        {children}
+      </ErrorBoundary>
+      </body>
     </html>
   )
 }
