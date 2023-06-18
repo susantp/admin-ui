a46fc0dbc0b5ae46ca8f98e96fc74445ff1d6e27
Susant Paudel <susanta.paudel@lisnepal.com.np>
2023-06-19 02:18:59 +0545

    Update: refactor middleware

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,6 @@
-import {getToken, JWT} from "next-auth/jwt";
+import {getToken} from "next-auth/jwt";
 import {NextRequest, NextResponse} from "next/server";
 
-const requireAuth: string[] = ["/dashboard"];
-
-const checkRoleActions = (token: JWT | null, pathname: string, request: NextRequest): NextResponse | undefined => {
-  if (
-    ['edit', 'delete', 'create'].includes(pathname) &&
-    !["admin", "manager"].includes(token?.role as string)
-  ) {
-    const url = new URL(`/403`, request.url);
-    return NextResponse.rewrite(url);
-  }
-  return undefined
-}
-const checkUnauthenticated = (token: JWT | null, request: NextRequest): NextResponse | undefined => {
-  if (!token) {
-    const url = new URL(`/api/auth/signin`, request.url);
-    url.searchParams.set("callbackUrl", encodeURI(request.url));
-    return NextResponse.redirect(url);
-  }
-  return undefined
-}
 
 export const config = {
   matcher: [
@@ -34,6 +14,7 @@ export const config = {
     "/((?!api|_next/static|_next/image|favicon.ico|monitoring|login|register).*)",
   ],
 }
+
 export async function middleware(request: NextRequest) {
   const res = NextResponse.next();
   const {pathname} = request.nextUrl;
@@ -42,9 +23,15 @@ export async function middleware(request: NextRequest) {
     secret: process.env.SECRET,
   });
 
-  if (requireAuth.some((path) => pathname.startsWith(path))) {
-    checkUnauthenticated(token, request)
-    checkRoleActions(token, pathname, request)
+  const unAuthenticatedRedirect = new URL(`/login`, request.url);
+  unAuthenticatedRedirect.searchParams.set("callbackUrl", encodeURI(request.url))
+  const unAuthorizedRewrite = new URL(`/403`, request.url)
+  if (!token) {
+    return NextResponse.redirect(unAuthenticatedRedirect);
+  }
+  if (['edit', 'delete', 'create'].includes(pathname) &&
+    !["admin", "manager"].includes(token.role as string)) {
+    return NextResponse.rewrite(unAuthorizedRewrite)
   }
   return res;
 }
