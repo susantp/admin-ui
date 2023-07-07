f530391e7e1ab79a674e5cae76072fdedf100e49
Susant Paudel <susanta.paudel@lisnepal.com.np>
2023-07-07 22:42:39 +0545

    Update: encapsulate helper functions

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,15 +1,27 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {type ClassValue, clsx} from "clsx"
+import {twMerge} from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]): string {
-  return twMerge(clsx(inputs))
+interface InterfaceGetHelpers {
+  joinClasses: (...classes: string[]) => string;
+  formatDate: (input: string | number) => string;
+  cn: (...inputs: ClassValue[]) => string;
 }
 
-export function formatDate(input: string | number): string {
-  const date: Date = new Date(input)
-  return date.toLocaleDateString("en-US", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  })
+const getHelpers = (): InterfaceGetHelpers => {
+  const cn = (...inputs: ClassValue[]): string => {
+    return twMerge(clsx(inputs))
+  }
+  const joinClasses = (...classes: string[]): string => {
+    return classes.filter(Boolean).join(" ");
+  }
+  const formatDate = (input: string | number): string => {
+    const date: Date = new Date(input)
+    return date.toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    })
+  }
+  return {joinClasses, formatDate, cn}
 }
+export default getHelpers
