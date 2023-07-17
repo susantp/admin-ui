2b25c9e0073e141c9d58b9f6e827f83010d03b36
Susant Paudel <susanta.paudel@lisnepal.com.np>
2023-07-17 15:35:00 +0545

    chore: roles crud

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -24,8 +24,6 @@ async function RootLayout({
   children,
 }: IRootLayout): Promise<JSX.Element | null> {
   const session: Session | null = await getClient.validateSession()
-
-  return null
   const userScreens: IScreen[] | null =
     await globalServices.fetchUserScreens()
   if (!session || !userScreens) return null
