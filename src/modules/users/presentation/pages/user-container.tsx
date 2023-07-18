6fd684f7492a16e2881a75675642318f7050f4d6
Susant Paudel <techbizznepal@gmail.com>
2023-07-18 17:49:08 +0545

    added create button

--- src/modules/users/presentation/pages/user-container.tsx
+++ src/modules/users/presentation/pages/user-container.tsx
@@ -1,18 +1,27 @@
 "use client"
 
-import React, { useState } from "react"
-import useTempData from "@/src/modules/dashboard/data/datasources/dashboard-datasource"
+import React, {useState} from "react"
+import useTempData
+  from "@/src/modules/dashboard/data/datasources/dashboard-datasource"
 import Section from "@/src/modules/dashboard/presentation/components/section"
-import PocContainer from "@/src/modules/global/presentation/components/poc-container"
-import PocDialogBox from "@/src/modules/global/presentation/components/poc-dialog-box"
+import PocContainer
+  from "@/src/modules/global/presentation/components/poc-container"
+import PocDialogBox
+  from "@/src/modules/global/presentation/components/poc-dialog-box"
 import PocTable from "@/src/modules/global/presentation/components/poc-table"
-import PocTablePagination from "@/src/modules/global/presentation/components/poc-table-pagination"
-import PocTableTools from "@/src/modules/global/presentation/components/poc-table-tools"
-import PocTableWrapper from "@/src/modules/global/presentation/components/poc-table-wrapper"
-import CreateEditUserForm from "@/src/modules/users/presentation/components/create-edit-user-form"
+import PocTablePagination
+  from "@/src/modules/global/presentation/components/poc-table-pagination"
+import PocTableTools
+  from "@/src/modules/global/presentation/components/poc-table-tools"
+import PocTableWrapper
+  from "@/src/modules/global/presentation/components/poc-table-wrapper"
+import CreateEditUserForm
+  from "@/src/modules/users/presentation/components/create-edit-user-form"
+import CreateButton
+  from "@/src/modules/users/presentation/components/create-button";
 
 export default function UserContainer(): JSX.Element {
-  const { people } = useTempData()
+  const {people} = useTempData()
   const [open, setOpen] = useState(false)
   const handleOnClose = (): void => {
     setOpen(!open)
@@ -23,17 +32,17 @@ export default function UserContainer(): JSX.Element {
         <Section
           label="User Listing"
           subLabel="A list of all the users in the system including their name, title, email and role."
-          actionEl={null}
+          actionEl={<CreateButton onClose={handleOnClose}/>}
         >
           <PocTableWrapper>
-            <PocTableTools />
-            <PocTable people={people} />
-            <PocTablePagination />
+            <PocTableTools/>
+            <PocTable people={people}/>
+            <PocTablePagination/>
           </PocTableWrapper>
         </Section>
       </PocContainer>
       <PocDialogBox open={open} onClose={handleOnClose}>
-        <CreateEditUserForm onClose={handleOnClose} />
+        <CreateEditUserForm onClose={handleOnClose}/>
       </PocDialogBox>
     </>
   )
