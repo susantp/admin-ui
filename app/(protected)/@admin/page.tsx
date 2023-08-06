d96f597a1a866186d691b2fe7fd58498453a075c
bishnulis <bishnu.das@lisnepal.com.np>
2023-08-06 21:09:12 +0545

    fix: null check roles and users permissions in dashboard

--- app/(protected)/@admin/page.tsx
+++ app/(protected)/@admin/page.tsx
@@ -28,71 +28,77 @@ export default async function Home(): Promise<ReactElement> {
   })
   return (
     <div className="flex flex-col h-full space-y-2">
-      <Card className="bg-secondary border-0">
-        <CardHeader>
-          <CardTitle>Top Roles</CardTitle>
-        </CardHeader>
-        <CardContent className="grid md:grid-cols-2 xl:grid-cols-3 gap-4">
-          {roles.map((role) => (
-            <Card className="flex place-items-center gap-4">
-              <CardHeader className="bg-primary-foreground rounded">
-                <CardTitle>{role.name.slice(0, 2)}</CardTitle>
-              </CardHeader>
-              <CardContent className="p-0 space-y-1">
-                <CardTitle>{role.name}</CardTitle>
-                <CardDescription>{role.members} user(s)</CardDescription>
-              </CardContent>
-            </Card>
-          ))}
-        </CardContent>
-      </Card>
+      {roles && (
+        <Card className="bg-secondary border-0">
+          <CardHeader>
+            <CardTitle>Top Roles</CardTitle>
+          </CardHeader>
+          <CardContent className="grid md:grid-cols-2 xl:grid-cols-3 gap-4">
+            {roles.map((role) => (
+              <Card className="flex place-items-center gap-4">
+                <CardHeader className="bg-primary-foreground rounded">
+                  <CardTitle>{role.name.slice(0, 2)}</CardTitle>
+                </CardHeader>
+                <CardContent className="p-0 space-y-1">
+                  <CardTitle>{role.name}</CardTitle>
+                  <CardDescription>{role.members} user(s)</CardDescription>
+                </CardContent>
+              </Card>
+            ))}
+          </CardContent>
+        </Card>
+      )}
 
-      <Card className="bg-secondary border-0 h-full">
-        <CardHeader>
-          <CardTitle>Top Users</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <Table className="bg-background rounded-md">
-            <TableHeader>
-              <TableRow>
-                <TableHead>Username</TableHead>
-                <TableHead>Name</TableHead>
-                <TableHead>Phone</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Role</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {users.results.map((user) => (
-                <TableRow key={user.id}>
-                  <TableCell>
-                    {user.username}
-                    <br />
-                    <span className="text-muted-foreground">{user.email}</span>
-                  </TableCell>
-                  <TableCell>
-                    {user.detail?.first_name} {user.detail?.last_name}
-                    <br />
-                    <span className="text-muted-foreground">
-                      {user.detail?.designation}
-                    </span>
-                  </TableCell>
-                  <TableCell>{user.phone}</TableCell>
-                  <TableCell>
-                    {user.is_active ? (
-                      <Badge>Active</Badge>
-                    ) : (
-                      <Badge variant="destructive">Inactive</Badge>
-                    )}
-                  </TableCell>
-
-                  <TableCell>{user.roles}</TableCell>
+      {users && (
+        <Card className="bg-secondary border-0 h-full">
+          <CardHeader>
+            <CardTitle>Top Users</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Table className="bg-background rounded-md">
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Username</TableHead>
+                  <TableHead>Name</TableHead>
+                  <TableHead>Phone</TableHead>
+                  <TableHead>Status</TableHead>
+                  <TableHead>Role</TableHead>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </CardContent>
-      </Card>
+              </TableHeader>
+              <TableBody>
+                {users.results.map((user) => (
+                  <TableRow key={user.id}>
+                    <TableCell>
+                      {user.username}
+                      <br />
+                      <span className="text-muted-foreground">
+                        {user.email}
+                      </span>
+                    </TableCell>
+                    <TableCell>
+                      {user.detail?.first_name} {user.detail?.last_name}
+                      <br />
+                      <span className="text-muted-foreground">
+                        {user.detail?.designation}
+                      </span>
+                    </TableCell>
+                    <TableCell>{user.phone}</TableCell>
+                    <TableCell>
+                      {user.is_active ? (
+                        <Badge>Active</Badge>
+                      ) : (
+                        <Badge variant="destructive">Inactive</Badge>
+                      )}
+                    </TableCell>
+
+                    <TableCell>{user.roles}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      )}
     </div>
   )
 }
