135f4a7d6dbd3e087fc6e17d47cadc5a52d14833
susant <gracysusant@gmail.com>
2024-08-30 04:07:17 +0545

    chore: refactor

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -3,17 +3,22 @@ import { NextRequest, NextResponse } from "next/server"
 import { actionSetAuthToken } from "@/modules/login/domain/auth-actions"
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
-  const callbackUrl = new URL(request.url)
-  const params = new URLSearchParams(callbackUrl.search)
-  const url = new URL(request.url)
+  const {
+    nextUrl: { searchParams },
+  } = request
+  const redirectUrl = new URL(request.nextUrl)
 
-  if (!params.get("token")) {
-    url.pathname = "/login"
-    return NextResponse.redirect(url)
+  if (!searchParams.get("token")) {
+    redirectUrl.pathname = "/login"
+    return NextResponse.redirect(redirectUrl)
   }
 
-  await actionSetAuthToken(params.get("token"))
-  url.searchParams.delete("token")
-  url.pathname = "/profile"
-  return NextResponse.redirect(url)
+  await actionSetAuthToken(searchParams.get("token"))
+  //TODO fetch user
+  //TODO set user to cookie
+
+  redirectUrl.searchParams.delete("token")
+  redirectUrl.pathname = "/profile"
+
+  return NextResponse.redirect(redirectUrl)
 }
